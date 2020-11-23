c6e70c6325d761ba47424b420665a5c09e95cf09
Jayakansal <21jayajindal@gmail.com>
2020-11-23 11:23:30 +0530

    This is for singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,6 +1,30 @@
-flip=$((RANDOM%2))
-if ((flip==1))
+#! /bin/bash
+
+echo -e "1: For Singlet \n2: For Doublet \n3: For Triplet"
+declare -A combinationDictionary
+
+function singlet()
+{
+num=$1
+        for ((counter=1; counter <= $num; counter++))
+        do
+                flip=$((RANDOM%2))
+                combinationDictionary[$flip]=$((${combinationDictionary[$flip]} +1))
+
+done
+singHeadProb=$(( (${combinationDictionary[0]}*100) / $num ))
+singTailProb=$(( (${combinationDictionary[1]}*100) / $num ))
+
+
+if (( $singHeadProb > $singTailProb ))
 then
-echo "It is a head!!"
+        echo "Head"
+        echo "Head = $singHeadProb %"
 else
-echo "It is a tail!!"
+        echo "Tail"
+        echo "Tail = $singTailProb %"
+fi
+
+        echo "Head: ${combinationDictionary[0]} Tail: ${singletDictionary[1]}"
+
+
